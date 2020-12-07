9041a1b8905e69360c496d02e5070090077e7110
VeraButler <verabutlerdesign@gmail.com>
2020-12-07 12:30:13 -0500

    update secret key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ['SECRET_KEY']
+# class BaseConfig:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = os.environ['SECRET_KEY']
